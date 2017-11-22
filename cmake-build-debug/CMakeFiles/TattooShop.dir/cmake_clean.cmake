b73ec6836012e94e347dcc77b00ffc1997a7651c
AnasGuetarni <anas.guetarni@gmail.com>
2017-11-22 15:32:08 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TattooShop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TattooShop.dir/main.c.o"
+  "TattooShop.pdb"
+  "TattooShop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TattooShop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
